113aa6313c62fa04bcf1f9ecbeaee56a4b0a457d
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-31 14:44:24 -0700

    Renaming python exercise file to main and adding logic to validate customer file

---
+++ validate_customer.py
@@ -0,0 +1,9 @@
+def validate_customer(value, column_name_1):
+
+    for i in value:
+        if i['column_name_1'] is None:
+            return False
+            continue
+        else:
+            return True
+        
