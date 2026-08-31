d3d37c82329063b2bab1e0095c2045fcbb84bebc
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-31 14:58:29 -0700

    Correcting code

--- validate_customer.py
+++ validate_customer.py
@@ -1,9 +1,25 @@
+"""
+order1 = {
+    "order_id": "1001",
+    "customer_id": "C001"
+}
+
+order2 = {
+    "order_id": "1003",
+    "customer_id": None
+}
+
+"""
+
 def validate_customer(value, column_name_1):
 
-    for i in value:
-        if i['column_name_1'] is None:
-            return False
-            continue
-        else:
-            return True
-        
+    if value.get(column_name_1) is None:
+        return False
+    else:
+        return True
+
+"""
+print(validate_customer(order1, "customer_id"))
+print(validate_customer(order2, "customer_id"))
+
+"""
