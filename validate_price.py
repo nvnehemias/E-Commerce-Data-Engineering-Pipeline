a1e6f4d0cb950df84a332f817f29e90520f83cca
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-31 15:18:01 -0700

    Fixing code

--- validate_price.py
+++ validate_price.py
@@ -1,30 +1,9 @@
-"""
-valid_order = {
-    "price": "899.99"
-}
-
-negative_order = {
-    "price": "-50.00"
-}
-
-bad_order = {
-    "price": "invalid"
-}
-"""
-
 def validate_price(value, column_name):
     try:
         price = float(value.get(column_name))
         if price < 0:
             return False 
         else:
-            True
+            return True
     except ValueError:
         return False
-    return True
-
-"""
-print(validate_price(valid_order,"price"))
-print(validate_price(negative_order,"price"))
-print(validate_price(bad_order,"price"))
-"""
