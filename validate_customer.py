4719847663254f817ba374db27362ba63c28f157
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-31 15:10:22 -0700

    Fixing code and adding to new file

--- validate_customer.py
+++ validate_customer.py
@@ -1,25 +1,6 @@
-"""
-order1 = {
-    "order_id": "1001",
-    "customer_id": "C001"
-}
-
-order2 = {
-    "order_id": "1003",
-    "customer_id": None
-}
-
-"""
-
 def validate_customer(value, column_name_1):
 
     if value.get(column_name_1) is None:
         return False
     else:
         return True
-
-"""
-print(validate_customer(order1, "customer_id"))
-print(validate_customer(order2, "customer_id"))
-
-"""
---
+++ validate_price.py
@@ -0,0 +1,27 @@
+valid_order = {
+    "price": "899.99"
+}
+
+negative_order = {
+    "price": "-50.00"
+}
+
+bad_order = {
+    "price": "invalid"
+}
+
+
+def validate_price(value, column_name):
+    try:
+        price = float(value.get(column_name))
+        if price < 0:
+            return False 
+        else:
+            True
+    except ValueError:
+        return False
+    return True
+
+print(validate_price(valid_order,"price"))
+print(validate_price(negative_order,"price"))
+print(validate_price(bad_order,"price"))
