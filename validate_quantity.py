e9ae234eca2ddc333d3778d83d0b028c0a339f5d
Nehemias Vicente <nvnehemias@gmail.com>
2026-08-31 21:55:40 -0700

    Changing float to int

--- validate_quantity.py
+++ validate_quantity.py
@@ -1,7 +1,7 @@
 def validate_quantity(value, column_name):
 
     try:
-        quantity = float(value.get(column_name))
+        quantity = int(value.get(column_name))
         if quantity <= 0:
             return False 
         else:
