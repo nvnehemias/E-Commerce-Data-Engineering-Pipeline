60230896a14febf762accd0f997942648a5339a8
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-04 22:27:55 -0700

    Adding test to test price

--- tests/test_price.py
+++ tests/test_price.py
@@ -8,7 +8,19 @@ sys.path.insert(0,str(project_root/"src"))
 
 from ecommerce_pipeline.validate_price import validate_price
 
-def test_price_exists():
+def test_price_does_exists():
+
+    validate_price_data = {
+        "customer_id": 101,
+        "price": 55.00,
+        "email": "alice@example.com"
+    }
+
+    result = validate_price(validate_price_data,"price")
+
+    assert result == True, f"Expected True for valid price: {result}"
+
+def test_price_does_not_exists():
 
     validate_price_data = {
         "customer_id": 101,
@@ -16,6 +28,31 @@ def test_price_exists():
         "email": "alice@example.com"
     }
 
-    result = validate_price(validate_price_data,"price")
+    result_2 = validate_price(validate_price_data,"price")
 
-    assert result == False, f"Expected False for invalid price: {result}"
+    assert result_2 == False, f"Expected False for non-existent price: {result_2}"
+
+def test_price_less_than_zero():
+
+    validate_price_data = {
+        "customer_id": 101,
+        "price": -20.00,
+        "email": "alice@example.com"
+    }
+
+    result_3 = validate_price(validate_price_data,"price")
+
+    assert result_3 == False, f"Expected False for invalide price: {result_3}"
+
+
+def test_price_integer():
+
+    validate_price_data = {
+        "customer_id": 101,
+        "price": "abc",
+        "email": "alice@example.com"
+    }
+
+    result_4 = validate_price(validate_price_data,"price")
+
+    assert result_4 == False, f"Expected False for integer price: {result_4}"
