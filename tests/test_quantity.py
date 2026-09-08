5e74bc681afc09512529bafdd16086bd3dea8dc6
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-07 20:08:38 -0700

    Adding test for quantity

--- tests/test_quantity.py
+++ tests/test_quantity.py
@@ -7,3 +7,15 @@ project_root = Path(__file__).resolve().parent.parent
 sys.path.insert(0,str(project_root/"src"))
 
 from ecommerce_pipeline.validate_quantity import validate_quantity
+
+def test_quantity_zero():
+
+    validate_quantity_data = {
+            "customer_id": 101,
+            "quantity": 0,
+            "email": "alice@example.com"
+        }
+    
+    result = validate_quantity(validate_quantity_data,"quantity")
+
+    assert result == False, f"Expected False for valid price: {result}"
