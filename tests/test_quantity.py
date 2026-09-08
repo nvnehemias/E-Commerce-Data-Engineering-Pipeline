f26a398dbc1412d9e2ca510a7d691eea19da1eef
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-07 20:09:39 -0700

    Adding another test

--- tests/test_quantity.py
+++ tests/test_quantity.py
@@ -19,3 +19,15 @@ def test_quantity_zero():
     result = validate_quantity(validate_quantity_data,"quantity")
 
     assert result == False, f"Expected False for valid price: {result}"
+
+def test_quantity_negative():
+
+    validate_quantity_data = {
+            "customer_id": 101,
+            "quantity": -10,
+            "email": "alice@example.com"
+        }
+    
+    result_2 = validate_quantity(validate_quantity_data,"quantity")
+
+    assert result_2 == False, f"Expected False for valid price: {result_2}"
