e899268b0811a683881da9c2c7a9e4765c2328e2
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-10 13:54:10 -0700

    Adding more tests

--- tests/test_convert_order.py
+++ tests/test_convert_order.py
@@ -43,3 +43,66 @@ def test_convert_order_output():
                 "quantity": 2,
                 "order_total": (89.99*2)
             }]
+
+def missing_customers():
+
+    dataset = [
+            {
+                "order_id": 1001,
+                "product": "Running Shoes",
+                "price": "89.99",
+                "quantity": "2"
+            }
+        ]
+
+    final_list_2, final_report_2 = convert_order(dataset)
+
+    for i in range(len(dataset)):
+    
+        assert final_report_2["total_orders"] == 1
+        assert final_report_2["successful_orders"] == 0
+        assert final_report_2["missing_customers"] == 1
+        assert final_report_2["invalid_price"] == 0
+        assert final_report_2["invalid_quantity"] == 0
+        assert final_report_2["duplicate_orders"] == 0
+
+    assert final_list_2 == []
+
+def invalid_quantity_check():
+
+    dataset_3 = [
+            {
+                "order_id": 1001,
+                "customer_id": 501,
+                "product": "Running Shoes",
+                "price": "89.99",
+                "quantity": "0"
+            },
+            {
+                "order_id": 1001,
+                "customer_id": 501,
+                "product": "Running Shoes",
+                "price": "89.99",
+                "quantity": "1"
+            }
+        ]
+
+    final_list_3, final_report_3 = convert_order(dataset_3)
+
+    assert final_report_3["total_orders"] == 2
+    assert final_report_3["successful_orders"] == 1
+    assert final_report_3["missing_customers"] == 0
+    assert final_report_3["invalid_price"] == 0
+    assert final_report_3["invalid_quantity"] == 1
+    assert final_report_3["duplicate_orders"] == 0
+
+    assert final_list_3 == [
+        {
+            "order_id": 1001,
+            "customer_id": 501,
+            "product": "Running Shoes",
+            "price": 89.99,
+            "quantity": 1,
+            "order_total": 89.99
+        }
+    ]
