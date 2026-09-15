54d2ec739d0b0c34e9ee7c8b91cf600dac8b22e6
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-15 13:37:41 -0700

    Adding modules and replacing code

--- connections/db_connection.py
+++ connections/db_connection.py
@@ -1,13 +1,16 @@
+import os
 import psycopg
+from dotenv import load_dotenv
+
+load_dotenv()
 
 try:
-        
     connection = psycopg.connect(
-        host = "127.0.0.1",
-        dbname= "ecommerce_pipeline",
-        user="postgres",
-        password="Gibson1paul",
-        port="5432"
+        host = os.getenv("DB_HOST","127.0.0.1"),
+        dbname = os.getenv("DB_NAME"),
+        user = os.getenv("DB_USER"),
+        password = os.getenv("DB_PASSWORD"),
+        port = os.getenv("DB_PORT","5432")
     )
 
     print("Successfully connected to the database!")
