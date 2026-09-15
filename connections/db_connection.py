2153d6d9c61f7d1387f472ceb5d9a1f11bba249c
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-15 14:35:30 -0700

    Adding to db connection file

--- connections/db_connection.py
+++ connections/db_connection.py
@@ -1,20 +1,26 @@
 import os
 import psycopg
+import logging
 from dotenv import load_dotenv
 
-load_dotenv()
 
-try:
-    connection = psycopg.connect(
-        host = os.getenv("DB_HOST","127.0.0.1"),
-        dbname = os.getenv("DB_NAME"),
-        user = os.getenv("DB_USER"),
-        password = os.getenv("DB_PASSWORD"),
-        port = os.getenv("DB_PORT","5432")
-    )
+def get_connection():
 
-    connection.close()
-    print("Successfully connected to the database!")
+    load_dotenv()
 
-except psycopg.Error as e:
-    print(f"Error connecting to PostgreSQL: {e}")
+    try:
+        connection = psycopg.connect(
+            host = os.getenv("DB_HOST","127.0.0.1"),
+            dbname = os.getenv("DB_NAME"),
+            user = os.getenv("DB_USER"),
+            password = os.getenv("DB_PASSWORD"),
+            port = os.getenv("DB_PORT","5432")
+        )
+
+        connection.close()
+        print("Successfully connected to the database!")
+
+    except psycopg.Error as e:
+        print(f"Error connecting to PostgreSQL: {e}")
+
+    return connection
