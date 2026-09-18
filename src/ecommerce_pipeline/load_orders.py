9722a885748cf822f12709e4f97369e410fe9c19
Nehemias Vicente <nvnehemias@gmail.com>
2026-09-18 16:48:16 -0700

    Adding on conflict to sql

--- src/ecommerce_pipeline/load_orders.py
+++ src/ecommerce_pipeline/load_orders.py
@@ -45,6 +45,7 @@ for f in json_files:
             sql_statement = """
                             insert into orders (order_id, customer_id, product, price, quantity, order_total)
                             values (%s,%s,%s,%s,%s,%s)
+                            on conflict (order_id) do nothing;
                             """
             for i in data:
                  values = (
